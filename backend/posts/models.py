7ef9ed03ffaff3ad5b955bdaa5c6f5ebe9f876f0
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-02-23 21:31:38 +0200

    feat: Add `save` method to `Post` model to manage post scheduling and `share_at` updates.

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -16,3 +16,17 @@ class Post(models.Model):
     shared_at_linkedin = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # pre-save
+        do_schedule_post = False
+        if all([
+            self.share_now is not None or self.share_at is not None,
+            self.share_complete_at is None and self.share_start_at is None
+        ]):
+            do_schedule_post = True
+
+        if self.share_now:
+            self.share_at = timezone.now()
+        super().save(*args, **kwargs)
+        # post-save
