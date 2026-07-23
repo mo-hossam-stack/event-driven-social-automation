19d5a2e200b2da491b213e54988905602ad97760
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-07-23 15:56:00 +0300

    refactor: update Inngest client logger to use module-specific logger for improved logging context

--- backend/scheduler/client.py
+++ backend/scheduler/client.py
@@ -2,7 +2,10 @@ import logging
 import inngest
 
 # Create an Inngest client
+
+logger = logging.getLogger(__name__)
+
 inngest_client = inngest.Inngest(
     app_id="social_share",
-    logger=logging.getLogger("gunicorn"),
+    logger=logger,
 )
